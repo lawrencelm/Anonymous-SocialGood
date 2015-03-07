df55251bcf6f4f1358e9a008b38074fd47452715
lawrencelm <lmurata@stanford.edu>
2015-03-07 03:16:59 -0800

    made new scheme to fix simulator bug

--- Smashtag/AppDelegate.swift
+++ Smashtag/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        var data = Data()
+        var data = BackendData()
         Parse.setApplicationId(data.applicationID, clientKey: data.clientKey)
         return true
     }
